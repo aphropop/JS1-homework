d7960bb851d3797835b0c382980caa11d5aecf5a
aphropop <52819351+aphropop@users.noreply.github.com>
2020-07-15 12:41:28 +0300

    Добавлено 2 файла
    1-файл html
    2-файл с js

---
+++ main.js
@@ -0,0 +1,17 @@
+//Первое задание
+let temperatureC = 37
+let temperatureF = (9 / 5) * temperatureC + 32
+alert ("Если температура по Цельсию = " + temperatureC + ", то по Фарингейту это равняется " + temperatureF.toFixed(2))
+
+//Второе задание
+let name = "Василий"
+let admin = name
+console.log (admin)
+
+//Третье задание отсутствует
+
+//Четвертое задание
+var fourTask = 1000 + "108"
+console.log (fourTask + " - тип переменной " + typeof fourTask)
+
+//Пятое задание - Изучено!
